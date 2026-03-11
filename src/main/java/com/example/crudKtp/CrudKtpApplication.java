93779ca1a05b58dcf1015f7732509cd23a963ff7
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2026-03-11 16:27:51 +0700

    add first project

---
+++ src/main/java/com/example/crudKtp/CrudKtpApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudKtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudKtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudKtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudKtp/CrudKtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudKtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudKtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
